0cb025518a78771455dc63562dc773ea8a2d3054
Juan Carlos Arana <aranacarvallo@hotmail.com>
2023-10-20 10:16:07 -0500

    Pay Mode Repository - Search

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.Data.SqlClient;
+using Supermarket_MVP.Properties;
+using Supermarket_MVP._Repositories;
+using Supermarket_MVP.Models;
 using Supermarket_MVP.Views;
+using Supermarket_MVP.Presenters;
 
 namespace Supermarket_MVP
 {
@@ -13,6 +18,10 @@ namespace Supermarket_MVP
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            string sqlConnectionString = Settings.Default.SqlConnection; 
+            IPayModeView view = new PayModeView();
+            IPayModeRepository repository = new PayModeRepository(sqlConnectionString);
+            new PayModePresenter(view, repository);
             Application.Run(new PayModeView());
         }
     }
